7b126cbe486e222274ee393291588079d8cb8762
WLAXY <WLAXY@DESKTOP-3CSLSJ5>
2023-05-13 21:38:54 -0500

    Agregar archivos de proyecto.

---
+++ citasmedicas/Login.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace citasmedicas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+
+            string usuario = txtUsuario.Text;
+            string contrasena = txtContrasena.Text;
+            if (usuario == "estudiante2023" && contrasena == "uisrael2023")
+            {
+                await Navigation.PushAsync(new Ingreso());
+            }
+            else
+            {
+                //bota la alerta y se limpia el cuadro 
+                DisplayAlert("ALERTA", "Usuario/Contraseña Incorrectos", "Cerrar");
+                txtUsuario.Text = "";
+                txtContrasena.Text = "";
+            }
+
+        }
+
+        private async void btnRegistro_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Reseteol());
+        }
+
+        private void btnOcontrasena_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
